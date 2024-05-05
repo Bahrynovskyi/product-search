46f25c60ff59758df5bb81eac3bb270bf9cd8ada
Bahrynovskyi <167538509+Bahrynovskyi@users.noreply.github.com>
2024-05-05 17:50:52 +0300

    Add files via upload
    Created "Contacts" page

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import "./Products.css";
 import useGetData from "../../hooks/useGetData";
 import Product from "../../components/Product/Product";
+import { useState } from "react";
 
 const Products = () => {
   const [data] = useGetData();
-  console.log(data);
+
+  const [searchOptions, setSearchOptions] = useState({});
+  const [productsToShow, setProductsToShow] = useState(data);
+
   return (
     <div className="products-wrapper">
+      <div className="products-filtration">adsda</div>
       <div className="products-items">
-        {data.map((item) => (
+        {productsToShow.map((item) => (
           <Product
             key={item.product_id}
             name={item.product_name}
